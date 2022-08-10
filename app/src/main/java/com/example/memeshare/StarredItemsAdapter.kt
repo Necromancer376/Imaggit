814d14eaffe6ba53695383ff5e049d498bf2a8cc
Malhar Dharmadhikari <66357015+Necromancer376@users.noreply.github.com>
2022-08-10 22:36:28 +0530

    changing image loading

--- app/src/main/java/com/example/memeshare/StarredItemsAdapter.kt
+++ app/src/main/java/com/example/memeshare/StarredItemsAdapter.kt
@@ -1,17 +1,29 @@
 package com.example.memeshare
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.icu.number.NumberFormatter.with
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide.with
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.item_starred.view.*
 
 class StarredItemsAdapter(
     private val context: Context,
     private val list: ArrayList<String>
 ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(
             LayoutInflater.from(context).inflate(
@@ -25,12 +37,17 @@ class StarredItemsAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
         val currentUrl = list[position]
+        val handler = Handler(Looper.getMainLooper())
 
         if(holder is MyViewHolder) {
-            Glide
-                .with(context)
-                .load(currentUrl)
-                .into(holder.itemView.imgItemStarred)
+//            GlideApp
+//                .with(context)
+//                .load(currentUrl)
+//                .centerCrop()
+//                .override(200, 200)
+//                .into(holder.itemView.imgItemStarred)
+
+            
         }
     }
 
