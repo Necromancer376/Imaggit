59a4b9f05be716dcb31d1f3b5ef9d2681e5002f8
Malhar Dharmadhikari <66357015+Necromancer376@users.noreply.github.com>
2022-08-30 16:42:55 +0530

    adding swipe gestures

--- app/src/main/java/com/example/memeshare/FullImageActivity.kt
+++ app/src/main/java/com/example/memeshare/FullImageActivity.kt
@@ -6,12 +6,14 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.activity_full_image.*
 
 
-class FullImageActivity : AppCompatActivity() {
+class FullImageActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
     var currentImageUrl = ""
     var starredItems = ArrayList<String>()
@@ -42,6 +44,7 @@ class FullImageActivity : AppCompatActivity() {
             startActivity(Intent(this@FullImageActivity,  StarredImagesActivity::class.java))
             finish()
         }
+
     }
 
     private fun setupActionBar() {
@@ -81,5 +84,30 @@ class FullImageActivity : AppCompatActivity() {
         if (value.isNullOrBlank())
             return defValue
         return ArrayList (value.split(",").map { it })
+    }
+
+    // Gesture
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return false
     }
 }
